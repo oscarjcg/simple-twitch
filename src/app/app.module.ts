e8d5e2186e3c1770cc27c0875b743affc79ad328
Oscar Catari Gutierrez <oscarcatari@outlook.es>
2019-10-16 19:03:12 +0100

    basic header done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { DiscoverComponent } from './discover/discover.component';
+import { BrowseComponent } from './browse/browse.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DiscoverComponent,
+    BrowseComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
